b6ef6dbe548f8a467c3347d733ea47a8ed015dcf
Shehab Ahamed Chowdhury Mitul <121333557+Mitul3047@users.noreply.github.com>
2023-11-17 21:22:57 +0600

    delete api created

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
+        const userCollection = client.db("bistroDb").collection("users");
         const menuCollection = client.db("bistroDb").collection("menu");
         const reviewCollection = client.db("bistroDb").collection("reviews");
         const cartCollection = client.db("bistroDb").collection("carts");
@@ -67,6 +68,13 @@ async function run() {
             const result = await cartCollection.insertOne(cartItem);
             res.send(result);
           });
+
+          app.delete('/carts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await cartCollection.deleteOne(query);
+            res.send(result);
+          })
 
 
         // Send a ping to confirm a successful connection
